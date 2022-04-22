9d586b84f91b367b41edb1ed4218039c9d3ddddd
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-04-22 00:11:36 -0700

    1 sub-directory [2 files]
    1 sub-directory for this directory, containing 2 files in Shell format.

---
+++ Dependencies/sh/DEPENDENCIES_V1_V1.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# Start of script
+# Seanpm2001's GitHub Backup Tool
+# Dependency list
+# Git (2020 or later)"
+# Git LFS"
+# The language Git LFS is written in (need an alternative that isn't written in Google Go)"
+# Curl"
+# gitHub.com"
+# api.github.com"
+# github.com/seanpm2001/"
+# Bash Shell"
+echo "Seanpm2001's GitHub Backup Tool\nDependency list\n"
+echo "Git (2020 or later)"
+echo "Git LFS"
+echo "The language Git LFS is written in (need an alternative that isn't written in Google Go)"
+echo "Curl"
+echo "gitHub.com"
+echo "api.github.com"
+echo "github.com/seanpm2001/"
+echo "Bash Shell"
+break
+# File info
+# File type: Bash Shell script file (*.sh)
+# File version: 1 (2022, Thursday, April 21st at 12:02 am)
+# Line count (including blank lines and compiler line): 28
+# End of script
+
