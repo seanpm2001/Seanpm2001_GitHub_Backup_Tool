1a17c5d7bd0e6249f4da86748b3d771e0b2420f2
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-04-22 00:19:10 -0700

    V1_GIT-LFS.p.go
    An archive of the first version of the Git LFS install demo script.

---
+++ OldVersions/Git-LFS/P/Go/V1_GIT-LFS.p.go
@@ -0,0 +1,12 @@
+// Start of script
+// Temporary demo GOogle Go script for getting Git-LFS
+// This is NOT a functional Google Go script
+// Go-Get Git-LFS
+go "Git-LFS" {
+  return "https://git-lfs.github.com/";
+};
+// File info
+// File type: Google Go source file (*.go)
+// File version: 1 (2022, Friday, April 22nd at 12:18 am PST)
+// Line count (including blank lines and compiler line): 13
+// End of script
